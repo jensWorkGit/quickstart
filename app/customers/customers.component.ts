5ceb7295d3e92b1011d71e365899f5017a94f1e8
jhoffmann <jens.hoffmann@lidl.com>
2016-06-23 14:06:27 +0200

    customers component added.

---
+++ app/customers/customers.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-customers',
+    templateUrl: 'app/customers/customers.component.html'
+})
+export class CustomersComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
