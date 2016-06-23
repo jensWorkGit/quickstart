5c3596dd140ffdc274abaa12cb3c81b8d16f23e6
jhoffmann <jens.hoffmann@lidl.com>
2016-06-23 17:14:38 +0200

    added module name

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {CustomersComponent} from './customer/customers.component';
 
 @Component({
-    moduleId: __moduleName,
+    moduleId: module.id,
     selector: 'my-app',
     templateUrl: 'app.component.html',
     directives: [CustomersComponent]
--- app/customer/customers.component.ts
+++ app/customer/customers.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    moduleId: __moduleName,
+    moduleId: module.id,
     selector: 'app-customers',
     templateUrl: 'customers.component.html'
 })
